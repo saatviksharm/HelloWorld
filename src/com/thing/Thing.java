8b71ea282d37f6ac714d8f5b206e837618a61edb
Saatvik Sharma <saatvik.sharma@gmail.com>
2021-06-15 21:10:20 -0400

    Learned try and catch, for loop, String methods. Edited com.Thing

--- src/com/thing/Thing.java
+++ src/com/thing/Thing.java
@@ -14,5 +14,37 @@ public class Thing {
         System.out.println(lastName);
         System.out.println(!firstName.equals(lastName));
         System.out.println(lastName.equals("Sharma"));
+        String nullString = null;
+        try {
+            System.out.println(nullString.length());
+        }
+        catch(Exception e) {
+            System.out.println("There is a null string");
+        }
+        String fullName = "Prashant Sharma";
+
+        for (int i = 0; i < fullName.length(); i++) {
+            System.out.println(fullName.substring(i, i+1));
+        }
+
+        for (int i = 0; i <= 9; i++) {
+            System.out.println(i+1);
+        }
+        int space = fullName.indexOf(" ");
+        try {
+            System.out.println("First Name " + fullName.substring(0,space));
+        } catch (Exception e) {
+            System.out.println("User only has 1 name and first name is " +fullName);
+        }
+
+        try {
+            System.out.println("Last Name " + fullName.substring(space, fullName.length()));
+        } catch (Exception e) {
+            System.out.println("No last name");
+        }
+
+
+
+
     }
 }
